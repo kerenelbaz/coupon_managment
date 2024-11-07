389a13883e8992ec8fb4d3c3ccb76aa6a757e2fb
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2024-11-07 18:04:27 +0200

    Admin class
    Created admin class for database table connection

---
+++ server/CouponsManagement/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CouponsManagement.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
+
