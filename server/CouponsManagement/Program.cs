c54b08dc22ab21f413409b2535423933d07f8f29
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2024-11-11 11:10:39 +0200

    Enabled cors
    allowed cross origin request from react app - configured to http://localhost:3000

--- server/CouponsManagement/Program.cs
+++ server/CouponsManagement/Program.cs
@@ -4,6 +4,17 @@ using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -43,6 +54,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseSession();
+
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
 
 app.MapControllers();
